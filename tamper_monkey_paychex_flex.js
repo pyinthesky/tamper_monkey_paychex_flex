0377ef4d7d431b9b429a7c8d59f8b2277a52ca47
pyinthesky <pyinthesky@users.noreply.github.com>
2021-09-24 10:56:23 -0400

    add the file

---
+++ tamper_monkey_paychex_flex.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         PayChex
+// @namespace    https://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://myapps.paychex.com/landing_remote/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setTimeout(() => {
+        Array.from(document.getElementsByClassName('timeCardTruncate')).forEach((el) => {
+        const t = el.getAttribute('png-tooltip');
+
+
+
+            if(t) {
+                const str = t.replace(/(.*?\/){3}/, '');
+                el.textContent = str.replace(' /', '');
+                el.style = "overflow: visible";
+            }
+
+
+
+       });
+
+
+
+        Array.from(document.getElementsByClassName('punch-widget-container-margin')).forEach((el) => {
+            if(el.getAttribute('data-payxautoid')) {
+               el.remove();
+            }
+        });
+         Array.from(document.getElementsByClassName('timecard-in-out-col')).forEach((el) => el.remove() );
+         Array.from(document.getElementsByClassName('timecard-punch-type-col')).forEach((el) => el.remove() );
+
+
+
+    }, 10000);
+})();
